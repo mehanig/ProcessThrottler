00f05d4e1757523a155ddd06306667a6e31db704
mehanig <mehanig@gmail.com>
2020-02-29 21:45:37 +0100

    allow to throttle multipe processes from 1 run

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ It's a simple CPU throttler.
 It accepts 2 params:
 
 - `-pid`: the pid of process to throttle
+- `-pids`: Comma separated list of PIDs passed as JSON string array: `-pids='[1,2,3,4]'`
 - `-cpu`: amount of throttling value in % from 1 to 99. Should be of type `Int`
 
 It works by suspending and resuming the pid.
+If both `pids` and `pid` args provided, values will be merged and throttling will be applied for every process.
+
 Cross-platform work is made possible by using this: "github.com/shirou/gopsutil/process"
 
 This is not recommended for any production use.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -14,33 +15,64 @@ import (
 
 func main() {
 
-	pid := flag.Int("pid", 0, "PID of the process to throttle")
-	cpu := flag.Int("cpu", 100, "Percentage of CPU limit, from 0 to 100. Setting to >=100 will have no effect. Setting to 0 will freeze process. Setting <0 will kill process.")
+	pidArg := flag.Int("pid", 0, "PID of the process to throttle. If provided with -pids, will be merged with list of pids.")
+	pidsArg := flag.String("pids", "[]", "Comma separated list of PIDs of the processes to throttle. Example: -pids='[1,2,3,4]'. If provided with -pid, -pid value will be added to the list.")
+	cpu := flag.Int("cpu", 100, "Percentage of CPU limit, from 0 to 100. Setting to >=100 will have no effect. Setting to 0 will freeze processes. Setting <0 will kill processes.")
 	flag.Parse()
 
-	if *pid == 0 {
+	if *pidArg == 0 && *pidsArg == "" {
 		log.Fatal("Incorrect params")
 	}
 
-	p, pidErr := process.NewProcess(int32(*pid))
+	var procPids []int
+	err := json.Unmarshal([]byte(*pidsArg), &procPids)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if pidErr != nil {
-		log.Fatal(pidErr)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	if *pidArg != 0 {
+		procPids = append(procPids, *pidArg)
 	}
 
 	if *cpu >= 100 {
 		return
 	}
+
+	var processes []*process.Process
+
+	for _, pid := range procPids {
+		p, pidErr := process.NewProcess(int32(pid))
+
+		if pidErr != nil {
+			log.Fatal(pidErr)
+			return
+		}
+		processes = append(processes, p)
+	}
+
 	if *cpu <= 0 {
-		killResult := p.Kill()
-		if killResult != nil {
-			log.Fatal(killResult)
+		for _, proc := range processes {
+			killResult := proc.Kill()
+			if killResult != nil {
+				log.Fatal(killResult)
+			}
 		}
 		return
 	}
 
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
+	for _, p := range processes {
+		go func(proc *process.Process) {
+			for true {
+				proc.Suspend()
+				time.Sleep(time.Duration(10*(100-(*cpu))) * time.Microsecond)
+				proc.Resume()
+				time.Sleep(time.Duration(10*(*cpu)) * time.Microsecond)
+			}
+		}(p)
+	}
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
@@ -51,17 +83,12 @@ func main() {
 		done <- true
 	}()
 
-	go func() {
-		for true {
-			p.Suspend()
-			time.Sleep(time.Duration(10*(100-(*cpu))) * time.Microsecond)
-			p.Resume()
-			time.Sleep(time.Duration(10*(*cpu)) * time.Microsecond)
-		}
-	}()
-
 	fmt.Println("Send SIGINT to remove limit")
 	<-done
-	p.Resume()
+
+	for _, proc := range processes {
+		proc.Resume()
+	}
+
 	fmt.Println("Exiting and removing CPU limit")
 }
