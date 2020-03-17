375410e73dc47ccd1b8881f9c913d8697efc0741
mehanig <mehanig@gmail.com>
2020-03-17 16:19:03 +0100

    ProcessThrottler stops when all processes are dead

--- nonWinApi.go
+++ nonWinApi.go
@@ -3,22 +3,48 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/shirou/gopsutil/process"
 )
 
 func throttle(processes []*process.Process, cpu int) {
+	gather := make(chan bool, len(processes))
 	for _, p := range processes {
-		go func(proc *process.Process) {
-			for true {
-				proc.Suspend()
-				time.Sleep(time.Duration(10*(100-cpu)) * time.Microsecond)
-				proc.Resume()
-				time.Sleep(time.Duration(10*cpu) * time.Microsecond)
+		stopChan := make(chan bool, 1)
+		go func(proc *process.Process, stopChannel chan bool) {
+			for {
+				select {
+				case <-stopChannel:
+					break
+				default:
+					proc.Suspend()
+					time.Sleep(time.Duration(10*(100-cpu)) * time.Microsecond)
+					proc.Resume()
+					time.Sleep(time.Duration(10*cpu) * time.Microsecond)
+				}
 			}
-		}(p)
+		}(p, stopChan)
+
+		go func(proc *process.Process, stopChannel chan bool) {
+			for true {
+				time.Sleep(time.Duration(2) * time.Second)
+				_, err := proc.Status()
+				if err != nil {
+					stopChannel <- true
+					gather <- true
+				}
+			}
+		}(p, stopChan)
 	}
+
+	for range processes {
+		select {
+		case <-gather:
+		}
+	}
+	os.Exit(1)
 }
 
 func resumeSuspended(processes []*process.Process) {
